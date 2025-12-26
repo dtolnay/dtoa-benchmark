1aff0b5a6b17afa99ed44b571f9e2010d640f860
David Tolnay <dtolnay@gmail.com>
2025-12-25 19:36:42 -0800

    Link to teju bug report
    https://github.com/andrepd/teju-jagua-rs/issues/1

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,7 @@ static IMPLS: &[Impl] = &[
         f32: |value, f| f(ryu::Buffer::new().format_finite(value)),
         f64: |value, f| f(ryu::Buffer::new().format_finite(value)),
     },
-    #[cfg(not(miri))]
+    #[cfg(not(miri))] // https://github.com/andrepd/teju-jagua-rs/issues/1
     Impl {
         name: "teju",
         f32: |value, f| f(teju::Buffer::new().format_finite(value)),
